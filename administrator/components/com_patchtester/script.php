73f07810f2a428df762d2b888bf26d34fb6e08dd
Roland Dalmulder <contact@rolandd.com>
2020-04-04 09:00:19 +0200

    Remove atum template
    Signed-off-by: Roland Dalmulder <contact@rolandd.com>

--- administrator/components/com_patchtester/script.php
+++ administrator/components/com_patchtester/script.php
@@ -6,11 +6,8 @@
  * @license    GNU General Public License version 2 or later
  */
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Filesystem\Folder;
 use Joomla\CMS\Installer\Adapter\ComponentAdapter;
 use Joomla\CMS\Installer\InstallerScript;
-use Joomla\CMS\Language\Text;
 
 /**
  * Installation class to perform additional changes during install/uninstall/update
@@ -19,14 +16,6 @@ use Joomla\CMS\Language\Text;
  */
 class Com_PatchtesterInstallerScript extends InstallerScript
 {
-	/**
-	 * Array of templates with supported overrides
-	 *
-	 * @var    array
-	 * @since  2.0
-	 */
-	protected $templateOverrides = array('atum');
-
 	/**
 	 * Extension script constructor.
 	 *
@@ -52,73 +41,6 @@ class Com_PatchtesterInstallerScript extends InstallerScript
 	}
 
 	/**
-	 * Function to perform changes during install
-	 *
-	 * @param   ComponentAdapter  $parent  The class calling this method
-	 *
-	 * @return  void
-	 *
-	 * @since   2.0
-	 */
-	public function install($parent)
-	{
-		$this->copyLayouts();
-	}
-
-	/**
-	 * Function to perform changes during update
-	 *
-	 * @param   ComponentAdapter  $parent  The class calling this method
-	 *
-	 * @return  void
-	 *
-	 * @since   2.0
-	 */
-	public function update($parent)
-	{
-		$this->copyLayouts();
-	}
-
-	/**
-	 * Function to perform changes during uninstall
-	 *
-	 * @param   ComponentAdapter  $parent  The class calling this method
-	 *
-	 * @return  void
-	 *
-	 * @since   2.0
-	 */
-	public function uninstall($parent)
-	{
-		// Initialize the error array
-		$errorTemplates = array();
-
-		// Loop the supported templates
-		foreach ($this->templateOverrides as $template)
-		{
-			// Set the file paths
-			$tmplRoot       = JPATH_ADMINISTRATOR . '/templates/' . $template;
-			$overrideFolder = JPATH_ADMINISTRATOR . '/templates/' . $template . '/html/com_patchtester';
-
-			// Make sure the template is actually installed
-			if (is_dir($tmplRoot))
-			{
-				// If there's a failure in copying the overrides, log it to the error array
-				if (Folder::delete($overrideFolder))
-				{
-					$errorTemplates[] = ucfirst($template);
-				}
-			}
-		}
-
-		// If we couldn't remove any overrides, notify the user
-		if (count($errorTemplates) > 0)
-		{
-			Factory::getApplication()->enqueueMessage(Text::sprintf('COM_PATCHTESTER_COULD_NOT_REMOVE_OVERRIDES', implode(', ', $errorTemplates)));
-		}
-	}
-
-	/**
 	 * Function to perform changes during postflight
 	 *
 	 * @param   string            $type    The action being performed
@@ -131,50 +53,5 @@ class Com_PatchtesterInstallerScript extends InstallerScript
 	public function postflight($type, $parent)
 	{
 		$this->removeFiles();
-	}
-
-	/**
-	 * Function to copy layout overrides for core templates at install or update
-	 *
-	 * @return  void
-	 *
-	 * @since   2.0
-	 */
-	private function copyLayouts()
-	{
-		// Initialize the error array
-		$errorTemplates = array();
-
-		// Loop the supported templates
-		foreach ($this->templateOverrides as $template)
-		{
-			// Set the file paths
-			$source      = __DIR__ . '/' . $template;
-			$tmplRoot    = JPATH_ADMINISTRATOR . '/templates/' . $template;
-			$destination = JPATH_ADMINISTRATOR . '/templates/' . $template . '/html/com_patchtester';
-
-			// Make sure the template is actually installed
-			if (is_dir($tmplRoot))
-			{
-				// If there's a failure in copying the overrides, log it to the error array
-				try
-				{
-					if (Folder::copy($source, $destination, '', true))
-					{
-						$errorTemplates[] = ucfirst($template);
-					}
-				}
-				catch (RuntimeException $exception)
-				{
-					$errorTemplates[] = ucfirst($template);
-				}
-			}
-		}
-
-		// If we couldn't remove any overrides, notify the user
-		if (count($errorTemplates) > 0)
-		{
-			Factory::getApplication()->enqueueMessage(Text::sprintf('COM_PATCHTESTER_COULD_NOT_INSTALL_OVERRIDES', implode(', ', $errorTemplates)));
-		}
 	}
 }
